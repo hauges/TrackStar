933cf410ab454d30405e96a9f69961b364a7a8db
Eric Haug <hauges@rose-hulman.edu>
2016-11-01 13:02:50 -0400

    Fav players load on home screen

--- src/js/favoritePlayers.js
+++ src/js/favoritePlayers.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    var apiUrl = "http://localhost:3000/users/"
+    var apiUrl = "http://localhost:3000/"
     var username = sessionStorage.getItem("userName");
     //var username = "user" // this sould not be set here but this is meant for testing
     var user;
@@ -10,7 +10,7 @@
      */
     function getUser() {
         $.ajax({
-            url: apiUrl + "/username-" + username,
+            url: apiUrl + "users/username-" + username,
             type: 'GET',
             dataType: 'JSON',
             async: false,
@@ -27,6 +27,27 @@
         });
     }
 
+    function getPlayer(playerID) {
+        var ret;
+        $.ajax({
+            url: apiUrl + "players/id-" + playerID,
+            type: 'GET',
+            dataType: 'JSON',
+            async: false,
+            success: function (data) {
+                if(data) {
+                    ret = data[0];
+                } else {
+                    console.log('user could not be found');
+                }
+            },
+            error: function (request, status, error) {
+                console.log(error, status, request);
+            }
+        });
+        return ret;
+    }
+
     $(document).ready(function () {
         getUser();
         console.log(user);
@@ -39,7 +60,9 @@
         var favs = user.favorites;
         console.log(favs);
         favs.forEach(function(element) {
-            $favDiv.append('<p>' + element + '</p>')
+            var player = getPlayer(element);
+            console.log(player);
+            $favDiv.append('<p>' + player.lastName + ", " + player.firstName + '</p>')
         }, this);
     }
 
