bdff8dfa400ac50f54f9942a0577abcaf7e06712
Eric Haug <hauges@rose-hulman.edu>
2016-11-04 18:47:56 -0400

    seach finds all players

--- src/js/search.js
+++ src/js/search.js
@@ -2,46 +2,69 @@
  * Created by Logan Schiessle on 11/1/16.
  * handles the function of search.html in TrackStar
  */
-    "use strict";
-    var username = sessionStorage.getItem("userName");
-    console.log("session username: " + username);
-    var apiUrl = "http://localhost:3000/";
-    $("[name='search']").on('input', search());
+"use strict";
+var username = sessionStorage.getItem("userName");
+console.log("session username: " + username);
+var apiUrl = "http://localhost:3000/";
+var searchString = '';
+var $searchResults = $('#search-results');
 
 
-    function search() {
-        console.log('searching');
-        var players = [];
-        $.ajax({
-            url: apiUrl + "players/players-" + "Troy",
-            type: 'GET',
-            dataType: 'JSON',
-            success: function (data) {
-                console.log(data);
-                if(data) {
-                    players = data;
-                } else {
-                    console.log('Player could not be found');
-                }
-            },
-            error: function (request, status, error) {
-                console.log(error, status, request);
+function search() {
+    console.log('searching');
+    if(searchString == '') {
+        searchString = '______';
+    }
+    var players = [];
+    $.ajax({
+        url: apiUrl + "players/players-" + searchString,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function (data) {
+            if(data) {
+                displayResults(data);
+            } else {
+                console.log('Player could not be found');
             }
-        });
-        displayResults(players);
+        },
+        error: function (request, status, error) {
+            console.log(error, status, request);
+        }
+    });
+}
+
+function displayResults(players) {
+    $searchResults.empty();
+    players.forEach(function(player) {
+        console.log(player.name);
+        var $listedPlayer = $('<p>');
+        $listedPlayer.text(player.name);
+        console.log($listedPlayer);
+        $listedPlayer.appendTo('#search-results');
+        $listedPlayer.on('click', function() {
+            var id = player._id;
+            console.log(id);
+            addPlayer(id);
+        })
+    }, this);
+}
+
+// make an ajax call
+function addPlayer(playerId) {
+
+}
+
+    function initializeSearch() {
+        console.log('initializing');
+        
+    //    add more initialization later...
     }
 
-    function displayResults(players) {
-        var results = $('#search-results').empty();
-        console.log(players);
-    }
-
-     function initializeSearch() {
-         console.log('initializing');
-         
-     //    add more initialization later...
-     }
-
-    initializeSearch();
+initializeSearch();
+var searchBar = $(':input[name="search"]');
+searchBar.on('input', function() {
+    searchString = searchBar.val();
+    search();
+});
 
 
