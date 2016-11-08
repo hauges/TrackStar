36c7baa65aa0e88a7b83fdf289979a5aeb810414
Chuck729 <alexaca@rose-hulman.edu>
2016-11-08 18:35:52 -0500

    players already added have "added" class in search page

--- src/frontend/js/search.js
+++ src/frontend/js/search.js
@@ -12,7 +12,7 @@ var $searchResults = $('#searchResults');
 
 function search() {
     console.log('searching');
-    if(searchString == '') {
+    if (searchString == '') {
         searchString = '______';
     }
     var players = [];
@@ -21,7 +21,7 @@ function search() {
         type: 'GET',
         dataType: 'JSON',
         success: function (data) {
-            if(data) {
+            if (data) {
                 displayResults(data);
             } else {
                 console.log('Player could not be found');
@@ -35,55 +35,61 @@ function search() {
 
 function displayResults(players) {
     $searchResults.empty();
-    players.forEach(function(player) {
-        console.log(player.name);
+    var favoritePlayers = [];
+    //get current favorite players
+    $.ajax({
+        url: apiUrl + "users/favoritesFromUsername-" + username,
+        type: 'GET',
+        dataType: 'JSON',
+        async: false,
+        success: function (data) {
+            if (data) {
+                favoritePlayers = data;
+                // for (var x = 0; x < data.length; x++) {
+                //     if (data[x] == id) {
+                //         alreadyAdded = true;
+                //         return;
+                //     }
+                // }
+            } else {
+                console.log('user could not be found');
+            }
+        },
+        error: function (request, status, error) {
+            console.log(error, status, request);
+        }
+    });
+    players.forEach(function (player) {
+
+        //verify player is not already added
+        var alreadyAdded = false;
+
         var $listedPlayer = $('<li>');
+
+        for (var x = 0; x < favoritePlayers.length; x++) {
+            if (favoritePlayers[x] == player._id) {
+                alreadyAdded = true;
+                $listedPlayer.addClass("added");
+                break;
+            }
+        }
         $listedPlayer.text(player.name);
-        console.log($listedPlayer);
         $listedPlayer.appendTo('#searchResults');
-        $listedPlayer.on('click', function() {
+        $listedPlayer.on('click', function () {
             var id = player._id;
             $listedPlayer.addClass("added");
             //add checked logo here
-            console.log(id);
-            //verify player is not already added
-            var alreadyAdded = false;
-             $.ajax({
-            url: apiUrl + "users/favoritesFromUsername-" + username,
-            type: 'GET',
-            dataType: 'JSON',
-            async: false,
-            success: function (data) {
-                if(data) {
-                    console.log("Data: "  + data);
-                    for (var x = 0; x < data.length; x++) {
-                        if (data[x] == id) {
-                            alreadyAdded = true;
-                            return;
-                        }
-                    }
-                } else {
-                    console.log('user could not be found');
-                }
-            },
-            error: function (request, status, error) {
-                console.log(error, status, request);
+
+            if (!alreadyAdded) {
+                addPlayer(id)
             }
-        });
-        if (!alreadyAdded) 
-        {
-            addPlayer(id)
-        }
-
-
-            //addPlayer(id);
         })
     }, this);
 }
 
 // make an ajax call
 function addPlayer(playerId) {
-    var input = {'favorites' : playerId};
+    var input = { 'favorites': playerId };
     console.log(input);
     $.ajax({
         url: apiUrl + "users/favs-" + username, // users/addFav-
@@ -91,7 +97,7 @@ function addPlayer(playerId) {
         dataType: 'JSON',
         data: input,
         success: function () {
-                console.log('added');
+            console.log('added');
         },
         error: function (request, status, error) {
             console.log(error, status, request);
@@ -99,15 +105,15 @@ function addPlayer(playerId) {
     });
 }
 
-    function initializeSearch() {
-        console.log('initializing'); 
-        
+function initializeSearch() {
+    console.log('initializing');
+
     //    add more initialization later...
-    }
+}
 
 initializeSearch();
 var searchBar = $(':input[name="search"]');
-searchBar.on('input', function() {
+searchBar.on('input', function () {
     searchString = searchBar.val();
     search();
 });
